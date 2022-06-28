4bfc4eaef2ac6eaaa1aa84588113603faf77399f
Ujjwal Shrestha <shresthaujjwal69@gmail.com>
2022-06-28 22:04:38 +0545

    Updates

---
+++ precache-manifest.9942ed257ec4fc74171cf9975b160422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910b2256d00d4b9407bad23e14c4b853",
+    "url": "/Gallery/index.html"
+  },
+  {
+    "revision": "62f93a6ddb79eba0fd9b",
+    "url": "/Gallery/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "6fe30cee29e9847391c2",
+    "url": "/Gallery/static/js/2.1a2fcf25.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/Gallery/static/js/2.1a2fcf25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f93a6ddb79eba0fd9b",
+    "url": "/Gallery/static/js/main.41d02339.chunk.js"
+  },
+  {
+    "revision": "34d1d1d903d7336f4b57",
+    "url": "/Gallery/static/js/runtime-main.81dd1f4f.js"
+  }
+]);
